11407c2fd74464fa3ce09f28458bd05f3b9eb593
ihiro3 <imiki@mtf.biglobe.ne.jp>
2026-07-13 02:35:54 +0900

    OMOTENASHI用ホームページに修正

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://nature-club-site.vercel.app',
+  site: process.env.SITE_URL || 'https://nature-club-site.vercel.app',
   base: '/',
   output: 'static',
 
--- src/navigation.ts
+++ src/navigation.ts
@@ -11,46 +11,46 @@ export const headerData: {
   actions: CallToAction[];
 } = {
   links: [
-    { text: 'ホーム', href: '/#home' },
-    { text: '直近のイベント', href: '/#event' },
-    { text: '代表挨拶', href: '/#greeting' },
-    { text: '会則', href: '/rules' },
-    { text: '活動の足跡', href: '/history' },
+    { text: 'Home', href: '/#home' },
+    { text: 'Destinations', href: '/#destinations' },
+    { text: 'Experiences', href: '/#experiences' },
+    { text: 'About', href: '/#about' },
+    { text: 'Services', href: '/services' },
   ],
-  actions: [{ text: 'イベントに応募する', href: '/#event', variant: 'primary' }],
+  actions: [{ text: 'Contact Me', href: '/contact', variant: 'primary' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'メニュー',
+      title: 'Menu',
       links: [
-        { text: 'ホーム', href: '/#home' },
-        { text: '直近のイベント', href: '/#event' },
-        { text: '代表挨拶', href: '/#greeting' },
+        { text: 'Home', href: '/#home' },
+        { text: 'Destinations', href: '/#destinations' },
+        { text: 'Experiences', href: '/#experiences' },
       ],
     },
     {
-      title: '倶楽部について',
+      title: 'About',
       links: [
-        { text: '会則', href: '/rules' },
-        { text: '活動の足跡', href: '/history' },
-        { text: '主催：せいか自然観察倶楽部', href: '/' },
+        { text: 'About Me', href: '/about' },
+        { text: 'Services', href: '/services' },
+        { text: 'Contact', href: '/contact' },
       ],
     },
   ],
   secondaryLinks: [],
   socialLinks: [
     {
-      ariaLabel: 'Facebook',
-      icon: 'tabler:brand-facebook',
-      href: 'https://www.facebook.com/people/%E3%81%9B%E3%81%84%E3%81%8B%E8%87%AA%E7%84%B6%E8%A6%B3%E5%AF%9F%E5%80%B6%E6%A5%BD%E9%83%A8/100054270127652/',
+      ariaLabel: 'Instagram',
+      icon: 'tabler:brand-instagram',
+      href: 'https://instagram.com',
     },
     {
-      ariaLabel: 'X',
-      icon: 'tabler:brand-x',
-      href: 'https://x.com/sciencekeihanna',
+      ariaLabel: 'Facebook',
+      icon: 'tabler:brand-facebook',
+      href: 'https://facebook.com',
     },
   ],
-  footNote: '© 2026 せいか自然観察倶楽部. All rights reserved.',
+  footNote: '© 2026 OMOTENASHI - Miki Inoue. All rights reserved.',
 };
