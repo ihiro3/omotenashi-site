45b7b684a917a4de1c0206dff8adb0c61872a24b
ihiro3 <imiki@mtf.biglobe.ne.jp>
2026-07-16 08:39:43 +0900

    OMOTENASHI用ホームページに修正

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://nature-club-site.vercel.app',
+  site: 'https://omotenashi-site.pages.dev',
   base: '/',
   output: 'static',
 
